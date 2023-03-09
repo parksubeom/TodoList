26e079befcfd35a4fd1dc8d14003f8aaaf7f23dd
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-03-10 08:37:56 +0900

    feat:로컬스토리지 저장 및 기본 틀 구성

--- mytodo-app/src/component/ToDoListItem.jsx
+++ mytodo-app/src/component/ToDoListItem.jsx
@@ -1,17 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect, memo } from 'react';
 import styled from 'styled-components';
 
+const ToDoliBlock = styled.li`
+ width: 100%;
+ list-style: none;
+ padding: 10px 0;
+ margin: 5px 0;
+ background: rgba( 232, 201, 237, 0.35 );
+ box-shadow: 0 0 8px 0 rgba(0, 0, 20, 0.3);
+ 
+`;
 
-function ToDoListItem ({todo}) {
+const ToDotextBlock = styled.span`
+text-align: right;
+font-size: 20px;
+`;
 
-    const {id, text, checked} = todo;
-    return(
-        <li className="TodoListItem">
+const ToDodateBlock = styled.p`
+text-align: right;
+font-size: 12px;
+margin-right: 10px;
+`;
+const ToDoCheckBlock = styled.input`
+    
+`
+
+function ToDoListItem({ todo }) {
+
+    const { id, text, checked, date } = todo;
+    return (
+        <ToDoliBlock>
             <div className={('checkbox', { checked })}>
-                <div className="text">{text}</div>
+                <div className="text">
+                    <ToDotextBlock>{text}</ToDotextBlock>
+                    <ToDodateBlock>{date}까지</ToDodateBlock>
+                </div>
+
             </div>
-            
-        </li>
+
+        </ToDoliBlock>
     );
 }
 
